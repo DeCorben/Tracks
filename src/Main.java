83af995575f77b6cf3d0aec9aae2cf246adf7d38
DeCorben <32888104+DeCorben@users.noreply.github.com>
2018-12-19 20:00:06 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import com.blackmanatee.rawtest.*;
+
+public class Main extends RawTest
+{
+	public static void main(String[] args)
+	{
+		new Main();
+	}
+	
+	public Main(){
+		featureOne();
+	}
+	
+	private void featureOne(){
+		//
+	}
+}
